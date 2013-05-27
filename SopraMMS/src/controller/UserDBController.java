b80c8d6682e308d4cfff7a81d76d46eecff4c72c
Teresa <teresa.hirzle@uni-ulm.de>
2013-05-27 11:11:50 +0200

    showRepresentative - kleine Änderung

--- SopraMMS/src/controller/UserDBController.java
+++ SopraMMS/src/controller/UserDBController.java
@@ -19,15 +19,15 @@ public class UserDBController {
 	private static PreparedStatement pStatement;
 
 	// Lokale Datenbank
-	private static final String URL = "jdbc:mysql://localhost:3306/mms";
-	private static final String USER = "root";
-	private static final String PASSWORD = "";
+//	private static final String URL = "jdbc:mysql://localhost:3306/mms";
+//	private static final String USER = "root";
+//	private static final String PASSWORD = "";
 
 	// Datenbank auf db4free.net
-//	 private static final String URL =
-//	 "jdbc:mysql://db4free.net:3306/sopramms";
-//	 private static final String USER = "teamaccount";
-//	 private static final String PASSWORD = "6lsj7tdm";
+	 private static final String URL =
+	 "jdbc:mysql://db4free.net:3306/sopramms";
+	 private static final String USER = "teamaccount";
+	 private static final String PASSWORD = "6lsj7tdm";
 	//
 	private static final String DRIVER = "com.mysql.jdbc.Driver";
 	static final int NUMBEROFRIGHTS = 5;
