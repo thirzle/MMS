63aa85b6a167e3acedaa0a79bd2dbf6045349238
AJ <johann.albach@uni-ulm.de>
2013-05-27 11:04:59 +0200

    Modul Einträge haben minimale Änderungen
    kleine fixes an einigen Stellen...

--- SopraMMS/src/management/TextualEntry.java
+++ SopraMMS/src/management/TextualEntry.java
@@ -7,7 +7,7 @@ public class TextualEntry extends Entry {
 
 	// invoke this constructor if you create a class with already existing data
 	public TextualEntry(int version, String date, boolean classification,
-			boolean approvalstatus, boolean declined, int entryID, String text, String title) {
+			boolean approvalstatus, boolean declined, int entryID, String title, String text) {
 		// all parameters are related to abstract class Entry...
 		super(version, date, classification, approvalstatus, declined, entryID, title);
 		// actual data is text...
