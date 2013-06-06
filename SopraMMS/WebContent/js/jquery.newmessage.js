df1233573fe0b5da1ef4b3791fe9e72d7c41b0aa
Lisa <lisa.klecha@uni-ulm.de>
2013-06-06 14:35:36 +0200

    Benutzerfreundlichkeit angepasst & javadoc

--- SopraMMS/WebContent/js/jquery.newmessage.js
+++ SopraMMS/WebContent/js/jquery.newmessage.js
@@ -1,7 +1,10 @@
 var mailaddress = $("#mailaddress");
 var mailto = $("#mailto");
 
-
+/**
+ * @param Wert
+ * due to the input of the user in the text area, the function searches the appropriate email address in the drop
+ */
 function Eingabe(Wert)
 {
     for(i = 0; i < document.getElementById("mailaddress").options.length; i++)
@@ -14,6 +17,9 @@ function Eingabe(Wert)
     }
 }
 
+/**
+ * Transfers the content into the text field after selecting an email from the drop
+ */
 function uebertrag(){
 	var text = document.getElementById("mailaddress").options[document.getElementById("mailaddress").selectedIndex].value;
 	document.getElementById("mailto").value = text;
