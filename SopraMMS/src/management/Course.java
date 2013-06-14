c3e73a3a96a0245d2612849f6cbb56c0a6eb0dc6
David Uni-PC <lehr.david@uni-ulm.e>
2013-06-14 12:34:49 +0200

    Getter und Setter zu Course hinzugefügt

--- SopraMMS/src/management/Course.java
+++ SopraMMS/src/management/Course.java
@@ -26,4 +26,46 @@ public class Course {
 		return description+" "+degree;
 	}
 
+	public String getCourseID() {
+		return courseID;
+	}
+
+	public void setCourseID(String courseID) {
+		this.courseID = courseID;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDegree() {
+		return degree;
+	}
+
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+
+	public String getFaculty() {
+		return faculty;
+	}
+
+	public void setFaculty(String faculty) {
+		this.faculty = faculty;
+	}
+
+	public boolean isType() {
+		return type;
+	}
+
+	public void setType(boolean type) {
+		this.type = type;
+	}
+	
+	
+
 }
