eb81ecc62e78bdf289729c502b362869b1323dec
AJ <johann.albach@uni-ulm.de>
2013-06-27 14:48:49 +0200

    Configuration settings update and Docu.
    nur doku update und kommentare in der configuration.ini file

--- SopraMMS/src/sysconfig/Config.java
+++ SopraMMS/src/sysconfig/Config.java
@@ -1,27 +1,92 @@
 package sysconfig;
 
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLDecoder;
 
+/**
+ * This class stores static information about the system's configuration.
+ * <p>
+ * The following settings are currently stored in this class and are
+ * accessible as static objects:
+ * <ul>
+ * 	<li>
+ * 		{@link #system_path}
+ * 	<li>
+ * 		{@link #system_setting_path}
+ * 	<li>
+ * 		{@link #system_pdf_path}
+ * 	<li>
+ * 		{@link #system_database_url}
+ * 	<li>
+ * 		{@link #system_database_user}
+ * 	<li>
+ * 		{@link #system_database_password}
+ * 	<li>
+ * 		{@link #system_database_driver}
+ * </ul>
+ * 
+ * <p>
+ * Example to gain access to a specific setting:
+ * <p>
+ * <code>
+ * 	//returns the absolute path, the system is running in.<br>
+ * 	sysconfig.Config.system_path.getValue();<br>
+ * 	<br>
+ * 	//returns the relative path (relative to the system_path),<br>
+ * 	//the pdf documents are stored in.<br>
+ * 	sysconfig.Config.system_pdf_path.getValue();<br>
+ * <code>
+ * 
+ * @see Setting
+ * 
+ * @author AJ
+ *
+ */
 public class Config {
 
     // the only real final setting in this system...
+    /**
+     * The absolute system path, this system runs in.
+     */
     public final static Setting system_path = new Setting();
+    /**
+     * The relative path to the settings file folder ("configuration.ini")
+     * (in relation to the {@link #system_path}).
+     */
     public final static Setting system_setting_path = new Setting();
     
+    /**
+     * The relative path to the PDF document folder
+     * (in relation to the {@link #system_path}).
+     */
     public final static Setting system_pdf_path = new Setting();
+    /**
+     * The url to the database.
+     */
     public final static Setting system_database_url = new Setting();
+    /**
+     * The user name of the database.
+     */
     public final static Setting system_database_user = new Setting();
+    /**
+     * The password of the database.
+     */
     public final static Setting system_database_password = new Setting();
+    /**
+     * The driver of the database.
+     */
     public final static Setting system_database_driver = new Setting();
 
     // not so final settings:
-
+    /**
+     * Loads the config file and initalizes the system settings.
+     * <p>
+     * (Paths tested on Apache only)
+     */
     public void load_settings() {
 	System.out.println("	loading config...");
-	// dirty hack... (jumping a few folder up from the class crap ...)
+	//dirty hack... (jumping a few folder up from the class crap ...)
 	URL sys_path = this.getClass().getResource("../../../");
 	String decoded_sys_path;
 	try {
@@ -33,6 +98,8 @@ public class Config {
 	    System.out.println("	sys_path URL decoding failed... (unsupported)");
 	    return;
 	}
+	//dirty hack end
+	
 	system_path.setValue(decoded_sys_path);
 	System.out.println("	system_path: " + decoded_sys_path);
 
--- SopraMMS/src/sysconfig/Setting.java
+++ SopraMMS/src/sysconfig/Setting.java
@@ -1,16 +1,38 @@
 package sysconfig;
 
+/**
+ * Wrapper class for system settings.
+ * <p>
+ * Check the {@link Config} class for more info.
+ * 
+ * @author AJ
+ *
+ */
 public class Setting {
+    /**
+     * The setting value.
+     */
     private String value = "";
     
+    /**
+     * Empty constructor.
+     */
     public Setting(){
 	
     }
     
+    /**
+     * Sets the setting value.
+     * @param value
+     */
     public void setValue(String value){
 	this.value = value;
     }
     
+    /**
+     * Retrieves the setting value.
+     * @return The setting value.
+     */
     public String getValue(){
 	return this.value;
     }
